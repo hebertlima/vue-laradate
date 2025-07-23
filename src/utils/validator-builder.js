a78c1a9ea556edcbca35e83993292ff83a124943
Hebert Lima <hebert.dev@hotmail.com>
2025-07-22 21:44:19 -0300

    create build validator

---
+++ src/utils/validator-builder.js
@@ -0,0 +1,34 @@
+import { helpers } from '@vuelidate/validators'
+import { logger } from './logger'
+
+export function buildValidator(name, validator) {
+	if (!validator) {
+		logger.error(`Validator "${name}" is undefined.`)
+
+		return helpers.withMessage(
+			() => `Validator "${name}" not found`,
+			() => false
+		)
+	}
+
+	const validatorFn = validator.$validator || validator
+	const originalParams = validator.$params || {}
+
+	logger.info(originalParams)
+
+	const enrichedParams = { 
+		type: name,
+		...originalParams,
+	}
+
+	const messageFn = validator.$message || (() => `Validation failed for ${name}`)
+
+	for (const [key, val] of Object.entries(originalParams)) {
+		enrichedParams[key] = val
+	}
+
+	return helpers.withMessage(
+		messageFn,
+		helpers.withParams(enrichedParams, validatorFn)
+	)
+}
