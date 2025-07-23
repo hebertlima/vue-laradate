4ea2ba825cb4a44215bbed5d5bd756f3bb4e4dd6
Hebert Lima <hebert.dev@hotmail.com>
2025-07-22 21:43:54 -0300

    trim

--- src/validators/proxies.js
+++ src/validators/proxies.js
@@ -5,6 +5,7 @@ export const createI18nProxies = (i18n) => {
 	const withI18nMessage = createI18nMessage({
 		t(key, params) {
 			try {
+
 				if (params?.property) {
 					const attributeKey = `attributes.${params.property}`
 					const attribute = i18n.t(attributeKey)
@@ -12,6 +13,7 @@ export const createI18nProxies = (i18n) => {
 						params.property = attribute
 					}
 				}
+
 				return i18n.t(key, params)
 			} catch (error) {
 				logger.warn(`Translation error for key "${key}":`, error)
